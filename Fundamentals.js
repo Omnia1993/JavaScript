19571f9efeb062e62a7c771587b115a3506da513
omnia266 <omniaeid266@gmail.com>
2020-10-28 12:18:45 -0500

    first commit

---
+++ Fundamentals.js
@@ -0,0 +1,42 @@
+const ScoreDolphins=(96+108+89)/3;
+const ScoreKoalas=(88+91+110)/3;
+console.log(ScoreDolphins,ScoreDolphins);
+
+if(ScoreDolphins>ScoreKoalas){
+
+    console.log('Dophins win the trophy');
+}
+    
+else if(ScoreKoalas>ScoreDolphins)
+{
+console.log('Koalas win the trophy');
+
+}
+else if(ScoreDolphins===ScoreKoalas)
+{
+console.log("both win the trophy")
+}
+
+//BOUNS 1   
+ ScoreDolphins=(97+112+80)/3;
+ ScoreKoalas=(109+95+50)/3;
+console.log(ScoreDolphins,ScoreKoalas); 
+
+if(ScoreDolphins>ScoreKoalas&&ScoreKoalas >=100) 
+  {
+console.log('Dolphins win th etrohpy')
+  }  
+  else if(ScoreKoalas>ScoreDolphins&&ScoreKoalas>=100){
+console.log('koalas win the trophy')
+
+  }
+  else if(ScoreDolphins===ScoreKoalas&&ScoreDolphins>=100&&ScoreKoalas>=100){
+      console.log('Both win the trophy !')
+  }
+  else {
+
+    console.log('no one wins the trophy')
+  }
+
+
+
---
+++ function.js
@@ -0,0 +1,22 @@
+let fName='Gulnar';
+
+function firstName(fName)
+{
+     console.log("hi Darling")
+    return firstName;
+} 
+//let whatIsYourFirstName=myName('omnia');
+//console.log(whatIsYourFirstName);
+
+function FullName(myFirstName,myLastName)
+{
+
+    const myFirstName=firstName(myFirstName);
+    const myLastName=firstName(myFirstName);
+    
+    return myLastName;
+
+}
+
+
+
